9749c742bf13cacb724229c5fab425c46930c78b
Crescens <crescencio.ps.code@gmail.com>
2024-02-01 23:36:23 -0600

    Agregada funcionalidad de carga de archivo CSV

--- logic.py
+++ logic.py
@@ -1,3 +1,27 @@
-class Perceptron:
+from tkinter import filedialog
+import pandas as pd
+import os
+import numpy as np
+
+
+class Neuron:
     def __init__(self):
-        pass
+        self.matrix = None
+    
+    def upload_csv(self, label_response):
+        filename = filedialog.askopenfilename(filetypes=[("CSV files", "*.csv")])
+        if filename:
+            base_name = os.path.basename(filename)
+            label_response.configure(text=base_name)
+            label_response.update()
+            data = pd.read_csv(filename)
+            vector = data.values.flatten() 
+            num_rows = data.shape[0]
+            num_cols = data.shape[1]
+            self.matrix = np.reshape(vector, (num_rows, num_cols))
+    
+    def start(self):
+        if self.matrix is not None:
+            print("Starting...")
+        else:
+            print("No matrix loaded")
