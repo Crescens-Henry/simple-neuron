9b21a020cd595c7a85f8ff537a4644d4ff01e025
Crescens <crescencio.ps.code@gmail.com>
2024-02-03 23:35:30 -0600

    Eliminar comentarios innecesarios y líneas de código no utilizadas en Plots.py

--- Plots.py
+++ Plots.py
@@ -1,4 +1,3 @@
-# En Plots.py
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -6,7 +5,6 @@ import pandas as pd
 sns.set_theme(style="darkgrid")
 
 def plot_data(list_epoch):
-    # Descomponer los pesos en valores individuales y asociar cada uno con su época correspondiente
     data = []
     for epoch_info in list_epoch:
         weights = epoch_info['weights']
@@ -16,7 +14,6 @@ def plot_data(list_epoch):
 
     plt.figure(figsize=(20, 6))
 
-    # Dibujar una línea para cada peso, excluyendo el sesgo x0 y la salida y
     plt.subplot(1, 2, 1)
     for i in range(1, len(list_epoch[0]['weights'])):
         sns.lineplot(x='id', y='weight_'+str(i), data=df, label='weight '+str(i), sort=False)
@@ -24,7 +21,6 @@ def plot_data(list_epoch):
     plt.xlabel('Época')
     plt.ylabel('Pesos')
 
-    # Dibujar una línea para error_norma
     plt.subplot(1, 2, 2)
     sns.lineplot(x='id', y='error_norma', data=df, label='norma del error |e|', color='r', sort=False)
     plt.title('Norma del error en cada iteración')
